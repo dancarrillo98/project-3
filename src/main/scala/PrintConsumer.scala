7a6bc841e7febc361aa3f9983559e1c979e8d677
Charpal <charles.dang1994@gmail.com>
2021-11-23 09:14:30 -0800

    Begin consumer program

---
+++ src/main/scala/PrintConsumer.scala
@@ -0,0 +1,15 @@
+package scala
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+
+object PrintConsumer{
+    def printTopicData(df: DataFrame): Unit = {
+        df.writeStream
+          .outputMode("update")
+          .format("console")
+          .start()
+          .awaitTermination()
+    }
+}
---
+++ src/main/scala/SparkConsumer.scala
@@ -0,0 +1,14 @@
+package scala
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+object SparkConsumer{
+    def displayQualifiedLeadTotal(df: DataFrame): Unit = {
+        val outputDF = df.select(count("id"))
+        outputDF.show()
+    }
+
+    def writeDataFrameToFile(df: DataFrame, filePath: String): Unit = {
+        df.write.mode("append").csv(filePath)
+    }
+}

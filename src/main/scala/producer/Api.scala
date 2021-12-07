0384bd4f16f86521f06be4283cbae307fcf07325
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2021-12-07 14:52:02 -0500

    Removed comments, unused variables from Api.scala

--- src/main/scala/producer/Api.scala
+++ src/main/scala/producer/Api.scala
@@ -1,4 +1,4 @@
-package producer //going to be producer when added to main project
+package producer
 
 import org.apache.http.HttpEntity
 import org.apache.http.HttpResponse
@@ -8,11 +8,6 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
 
 object Api {
-
-
-    var tmpStr1 = """ id: 0, name: jj, lname: mer"""
-    var tmpStr = tmpStr1 + "\n" +tmpStr1 + "\n" +tmpStr1+"\n" + tmpStr1;
-    
 
     /**
       * 
@@ -46,24 +41,8 @@ object Api {
    
     final val apiKeyArray = Array("jksgn32","2f9538c0", "235571b0", "fe859db0", "d7f87c40")
     var keyIter = 0
-    var invalidKeyList = Array()
+    
     //Calls to Mockaroo API to generate mock data
-    
-    // Iterate through apiKeyArray until first key that produces a message
-
-    // def recruiterData(): Array[String] =    
-    //     for(key <- 0 to apiKeyArray.length){
-    //         try{
-    //             getRestContent(s"https://my.api.mockaroo.com/Recruiters?key=$key").split("},")
-    //         }catch{
-    //             case ex : Throwable => {
-    //             ex.printStackTrace();
-    //             println(s"API was not called because call limit for $key has been met")
-    //             throw new Exception (s"${ex.getMessage}")
-    //             }
-    //         }
-    //     }
-    
     val RecruiterURL        =   "https://my.api.mockaroo.com/Recruiters?key="
     val qlDataURL           =   "https://my.api.mockaroo.com/Qualified_Lead?key="
     val screenerDataURL     =   "https://my.api.mockaroo.com/Screeners?key="
@@ -119,34 +98,5 @@ object Api {
     def caData(): Array[String] =   obtainData(caDataURL)
 
     def qlBIGData(): Array[String] =   obtainData(qlBIGDataURL)
-
-    //def recruiterData(): Array[String] =    getRestContent(s"https://my.api.mockaroo.com/Recruiters?key=${apiKeyArray(2)}").split("},")
-
-    // def qlData(): Array[String] =           getRestContent(s"https://my.api.mockaroo.com/Qualified_Lead?key=${apiKeyArray(2)}").split("},")
-
-    // def screenerData(): Array[String] =     getRestContent(s"https://my.api.mockaroo.com/Screeners?key=${apiKeyArray(2)}").split("},")
-
-    // def offerData(): Array[String] =        getRestContent(s"https://my.api.mockaroo.com/Offers?key=${apiKeyArray(2)}").split("},")
-
-    // def screeningData(): Array[String] =    getRestContent(s"https://my.api.mockaroo.com/Screening?key=${apiKeyArray(2)}").split("},")
-
-    // def caData(): Array[String] =           getRestContent(s"https://my.api.mockaroo.com/Contact_Attempts?key=${apiKeyArray(2)}").split("},")
-
-    // def qlBIGData(): Array[String] =        getRestContent(s"https://my.api.mockaroo.com/Qualified_Lead_Big_Table?key=${apiKeyArray(2)}").split("},")
-
-//////
-    // def recruiterData(): Array[String] =    tmpStr.split("\n");
-
-    // def qlData(): Array[String] =           tmpStr.split("\n");
-
-    // def screenerData(): Array[String] =     tmpStr.split("\n");
-
-    // def offerData(): Array[String] =        tmpStr.split("\n");
-
-    // def screeningData(): Array[String] =    tmpStr.split("\n");
-
-    // def caData(): Array[String] =           tmpStr.split("\n");
-
-    // def qlBIGData(): Array[String] =        tmpStr.split("\n");
 
 }
